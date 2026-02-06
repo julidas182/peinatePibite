e822b8e23d891df57b3f95f4d93492d85defe7c2
Julian Dascenzo <jdascenzo@smata.com.ar>
2026-02-06 15:44:09 -0300

    modifico config.js

--- public/apis-env.js
+++ public/apis-env.js
@@ -2,4 +2,7 @@ window._CONFIG_ = {
   API_URL: 'https://api.example.com',
   TIMEOUT: 5000,
   //ACA PONEMOS TODO QUE DINAMICO QUE QUEREMOS
+  EMAILJS_SERVICE_ID: 'service_xxxxxx',
+  EMAILJS_TEMPLATE_ID: 'template_xxxxxx',
+  EMAILJS_PUBLIC_KEY: 'tu_public_key_xxxxxx'
 };
